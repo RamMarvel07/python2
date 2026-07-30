d598217f47687d68d15ab70f96063377e7a1bca6
ramdalavi <ramdalavi071@gmail.com>
2026-07-30 00:33:40 -0700

    Add files via upload

---
+++ pp1.py
@@ -0,0 +1,10 @@
+
+user_string = input("Enter a string: ")
+
+string_length = 0
+
+for character in user_string:
+   
+    string_length += 1
+
+print("The length of the string is :", string_length)
